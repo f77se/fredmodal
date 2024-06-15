57d11ddec560b00ab8158b3589dd783412dd2197
Michael Hansson <m@f77.se>
2024-06-15 17:26:16 +0200

    fixed close link to proper nav

--- app/routes/tables.$page.details.tsx
+++ app/routes/tables.$page.details.tsx
@@ -1,17 +1,15 @@
-import { useNavigate, useRouteLoaderData } from "@remix-run/react";
+import { Link, useRouteLoaderData } from "@remix-run/react";
 
 export default function TablePage() {
   const { page } = useRouteLoaderData("routes/tables.$page") as {
     page: number;
   };
 
-  const navigate = useNavigate();
-  const goBack = () => navigate(-1);
   return (
     <div>
       <h1>Modal view</h1>
       <p>Current page: {page}</p>
-      <button onClick={goBack}>Close</button>
+      <Link to={`/tables/${page}`}>Close</Link>
     </div>
   );
 }
